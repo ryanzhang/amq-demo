7f8da4f63865b4cecbed64250ffa7787802435ce
Gustavo Luszczynski <luszczynski@gmail.com>
2021-08-17 12:22:02 -0300

    add camel-k

---
+++ camel-k/AMQBrokerIntegration.java
@@ -0,0 +1,28 @@
+// camel-k: dependency=camel-activemq
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.BindToRegistry;
+
+public class AMQBrokerIntegration extends RouteBuilder {
+    @BindToRegistry
+    public ActiveMQConnectionFactory registerActiveMQConnectionFactory() {
+        System.out.println("ActiveMQ Listener: STARTING...");
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL("tcp://ex-aao-hdls-svc:61617");
+        connectionFactory.setUserName("admin");
+        connectionFactory.setPassword("redhat");
+        connectionFactory.setUseAsyncSend(false);
+        connectionFactory.setClientID("Message Topic");
+        connectionFactory.setConnectResponseTimeout(300);
+        System.out.println("ActiveMQ Listener: STARTED");
+        return connectionFactory;
+    }
+
+    @Override
+    public void configure() throws Exception {
+      from("activemq:topic:topic.prices")
+        .log("body = ${body}")
+        ;
+    }
+}
