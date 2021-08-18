ac6a362ebabecaae640697ea4b2f4a838d83f503
Gustavo Luszczynski <luszczynski@gmail.com>
2021-08-18 16:03:04 -0300

    increase doc

--- camel-k/AMQBrokerIntegration.java
+++ camel-k/AMQBrokerIntegration.java
@@ -9,7 +9,7 @@ public class AMQBrokerIntegration extends RouteBuilder {
     public ActiveMQConnectionFactory registerActiveMQConnectionFactory() {
         System.out.println("ActiveMQ Listener: STARTING...");
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-        connectionFactory.setBrokerURL("tcp://ex-aao-hdls-svc:61617");
+        connectionFactory.setBrokerURL("tcp://amq-broker-hdls-svc:61617");
         connectionFactory.setUserName("admin");
         connectionFactory.setPassword("redhat");
         connectionFactory.setUseAsyncSend(false);
